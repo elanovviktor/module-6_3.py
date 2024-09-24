caee639f370b5f2b744a57d181811466c8e9ee51
elanovviktor <elanov_viktor@mail.ru>
2024-09-24 19:43:30 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+class Horse:
+
+    x_distance = 0
+    sound = 'frrr'
+
+    def run(self, dx):
+        self.x_distance += dx
+        return self.x_distance
+
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repet'
+
+    def fly(self, dy):
+        self. y_distance += dy
+        return self. y_distance
+
+class Pegasus(Horse, Eagle):
+
+    def move(self, dx, dy):
+        super(). run(dx)
+        super(). fly(dy)
+    def get_pos(self):
+        peg = (self.x_distance, self.y_distance)
+        print(peg)
+    def voice(self):
+        print(Horse. sound)
+        print(Eagle. sound)
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
